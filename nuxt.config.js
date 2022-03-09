07b51eb977e761660cfc346e58d36ec5e1bbba41
shiibawataru <wataru.shiiba@rakus-partners.co.jp>
2022-03-09 16:14:53 +0900

    修正しました

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,3 @@
-import TerserPlugin from 'terser-webpack-plugin'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -35,14 +34,11 @@ export default {
       },
     ],
   },
-
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
-
   // Netlify デプロイのため
   ssr: true,
-  target: 'static',
-
+  target: 'server',
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/vue-cropper', mode: 'client' },
@@ -52,10 +48,8 @@ export default {
     { src: '~/plugins/swiper', mode: 'client' },
     // { src: '~/plugins/skelton', mode: 'client' },
   ],
-
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
@@ -63,33 +57,21 @@ export default {
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
   ],
-
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     ['cookie-universal-nuxt', { parseJSON: false }],
   ],
-
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
   },
-
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: ['amCharts'],
     vendor: ['vue-cropperjs'],
-    optimization: {
-      minimize: true,
-      minimizer: [
-        new TerserPlugin({
-          cache: true,
-          parallel: false,
-        }),
-      ],
-    },
   },
   // ローディング
   loading: '~/components/Loading.vue',
